58a347719c9ed7b3f774127243f7adabfbdbfa6c
NurzhanAuelbek <82052572+NurzhanAuelbek@users.noreply.github.com>
2023-07-11 03:53:41 +0600

    Добавьте файлы проекта.

---
+++ winformzip/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Forms;
+using Ionic.Zip;
+namespace winformzip
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Предлагает пользоваталю выбрат папку
+        FolderBrowserDialog bd = new FolderBrowserDialog();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //задает путь пользователя
+            if (bd.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = bd.SelectedPath;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Предлагает пользователя где сохранить файл
+            SaveFileDialog sfd = new SaveFileDialog();
+            //При сохранение определяет тип файла
+            sfd.Filter = "Zip files (*.zip)|*.zip";
+            if (textBox1.Text != "" && sfd.ShowDialog() == DialogResult.OK)
+            {
+                ZipFile zf = new ZipFile(sfd.FileName);
+                zf.AddDirectory(bd.SelectedPath);
+                zf.Save();
+                MessageBox.Show("Архивация прошла успешно.", "Выполнено");
+            }
+        }
+    }
+}
